1c2905d4fe1c0d8ae7a887e496eec37d20867444
Nattawat Nonsung <armmer1@gmail.com>
2014-03-06 21:50:04 +0700

    Create blankArea to replace fixtop element. This will make it smoother

--- jquery.fixtop.js
+++ jquery.fixtop.js
@@ -17,27 +17,37 @@
 		// Define default setting
 		var settings = $.extend({
 			marginTop: 0,
-			zIndex: 1
+			zIndex: 1000
     }, options );
 
 		var form_top = this.offset().top - settings.marginTop;
 		var el = this;
 		var missingHeight = el.height() + settings.marginTop;
+		var blankArea = $("<div/>")
+		blankArea.css({
+        'display' : el.css('display'),
+        'width' : el.outerWidth(true),
+        'height' : el.outerHeight(true),
+        'float' : el.css('float')
+    });
+
 	  $(window).scroll(function(e){ 
 	    //Set position of sub navogation
 	    var y = form_top;
 	    if ($(this).scrollTop() > y && el.css('position') != 'fixed'){ 
+	    	el.after(blankArea);
 	      el.css({
 	      	'position': 'fixed', 
 	      	'top': settings.marginTop+'px',
 	      	'z-index': settings.zIndex, 
-	      	'width': el.width()+'px' 
+	      	'width': "100%" 
 	      }); 
 	      if(settings.fixed !== undefined){
 	      	settings.fixed(el);
 	      }
 	    } 
 	    if ($(this).scrollTop() < y && el.css('position') == 'fixed'){
+	    	blankArea.remove();
 	    	el.css({
 	     		'position': 'relative', 
 	     		'top': '0px',
